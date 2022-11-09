56f44b20257872828cab31f3e8a0a35c65da2ace
rafaelly segantini <110680184+rivalzinha@users.noreply.github.com>
2022-11-09 10:32:53 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,14 +1,82 @@
-let tabuada = 6;
+function moeda(atual){
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+  }
+  
+  function total(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = moeda;
+  
+  
+    if(!Number(c)){
+        
+     alert("O capital deve ser numeros.");
+     document.getElementById("valor").value = "";
+     document.getElementById("valor").focus();
+     return;
+    }
+  
+    if(!Number(j)){   
+      alert("O juros deve ser numeros.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return;
+     }
+  
+     if(!Number(t)){
+      alert("O meses deve ser numeros.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+      return;
+     }
+   let mes = "";
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+  
+       mes += "Mês " + i + " / " + " valor: " +moeda(r) + "<br>";
+  
+        c = r;
+    }
+    document.getElementById("mes").innerHTML=mes
+  
+    document.getElementById("total").innerHTML="Total: "+moeda(r);
+    
+  }
+    
 
-function escreva(){
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
-}
+  function adicao() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) + Number(val2);
+    document.getElementById("resultado").innerHTML = r;
+  }
+
+  function subtracao() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) - Number(val2);
+    document.getElementById("resultado").innerHTML = r;
+  }
+
+  function divisao() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) / Number(val2);
+    document.getElementById("resultado").innerHTML = r;
+  }
+
+  function multiplicacao() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) * Number(val2);
+    document.getElementById("resultado").innerHTML = r;
+  }
+  function porcentagem() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let c = 100
+    let p = Number(val2) / Number(c)
+    let r = Number(val1) * Number(p);
+    document.getElementById("resultado").innerHTML = r;
+  }
